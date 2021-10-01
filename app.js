046afcea5d94d6ce55f8ce53eec03e307a5ec142
Roman <roman@kimaia.com>
2021-10-01 12:11:54 +0300

    set port from process env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json({limit: '50mb'}))
 // app.use(express.urlencoded())
